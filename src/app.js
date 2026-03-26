0ff892ee1ff206055603d7c0656e400b6abb5079
Anzelah <jepkoechanzellah@gmail.com>
2026-03-26 13:56:17 +0300

    wire everything together(so far) in app

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const StripeProvider = require('./providers/stripe.provider');
+const MpesaProvider = require('./providers/mpesa.provider');
+const PaymentService = require('./services/payment.service');
+
+const paymentService = new PaymentService({
+  stripe: new StripeProvider(),
+  mpesa: new MpesaProvider(),
+});
