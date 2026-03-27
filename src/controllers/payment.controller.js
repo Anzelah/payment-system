4a61deec2288f855476467cbdddaebc4026a5d2a
Anzelah <jepkoechanzellah@gmail.com>
2026-03-27 13:21:44 +0300

    switch the idempotency key to come from client

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -5,26 +5,25 @@ const { v4: uuidv4 } = require("uuid")
 async function createPayment (req, res) {
     try {
         // validate inputs
-        const { userId, amount, currency, provider } = req.body
+        const { userId, amount, currency, provider, idempotencyKey } = req.body
 
-        if (!userId || !amount || !currency || !provider ) { //to replace with zod validation
+        if (!userId || !amount || !currency || !provider ||!idempotencyKey ) { //to replace with zod validation
             return res.status(400).json({ error: "Missing required fields" })
         }
 
-        //create idempotency keys(temporary)
-        const idempotencyKey = uuidv4()
+        //check for double transaction 
         const existingTransaction = await prisma.transaction.findUnique({
             where: { idempotencyKey }
         })
         if (existingTransaction) {
-            return res.status(200).json({ message: "A similar transaction is underway"})
+            return res.status(200).json(existingTransaction)
         }
 
         // create the transaction in the db
         const transaction = await prisma.transaction.create({
             data: {
                 userId,
-                provider: provider.toUppercase(),
+                provider: provider.toUpperCase(),
                 reference: `ref_${Date.now()}`,
                 amount,
                 currency,
@@ -34,11 +33,20 @@ async function createPayment (req, res) {
         })
 
         // call payment service to process the payment now
-        const response = paymentService.createPayment(provider, { amount, currency, reference: transaction.reference } )
+        const response = await paymentService.createPayment(
+            provider,
+            { amount, currency, reference: transaction.reference } )
 
-        return res.status(201).json({ message: "Payment inititated succesfully", transaction, response })
+        return res.status(201).json({
+            message: "Payment inititated succesfully",
+            transaction, 
+            providerResponse: response
+        })
     } catch(error) {
         console.error(error)
         res.status(500).json({ error: "Something went wrong"})
     }
 }
+
+
+module.exports = new PaymentController();
