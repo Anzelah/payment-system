480f18ee840ebccb0b3a6d140cdd1502398ff6ef
Anzelah <jepkoechanzellah@gmail.com>
2026-03-31 12:24:44 +0300

    create a payment worker

--- src/workers/worker.js
+++ src/workers/worker.js
@@ -1,3 +1,18 @@
 const { Worker, Job } = require("bullmq")
 const paymentQueue = require("../queues/payment.queue")
+const connection = require("../utils/redis")
 
+const worker = new Worker('payment', 
+    async job => {
+        // process job here
+    },
+    { connection }
+);
+
+worker.on('completed', job => {
+    console.log(`Job ${job.id} completed!`)
+});
+
+worker.on('failed', (job, err) => {
+    console.log(`Job ${job.id} failed with ${err}`)
+});
