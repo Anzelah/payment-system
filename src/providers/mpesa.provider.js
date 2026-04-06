e2cc71596d1750475ca3b0683a896067fbb8d78d
Anzelah <jepkoechanzellah@gmail.com>
2026-04-06 23:05:02 +0300

    start mpesa implementation

--- src/providers/mpesa.provider.js
+++ src/providers/mpesa.provider.js
@@ -1,6 +1,7 @@
 // function to process mpesa payments. mpesa implementation and logic goes here
 class MpesaProvider {
     async createPayment(data) {
+      // Only implementation of the mpesa stk push(stripe-checout-session equivalent) is done here. the other is in webhook controller
       console.log("M-Pesa payment initiated", data);
   
       return {
