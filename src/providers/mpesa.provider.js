49e2ba2cc19e9597e423b9cbd4c0e30e8ec3147c
Anzelah <jepkoechanzellah@gmail.com>
2026-04-07 15:03:25 +0300

    implement generatepassword function for use in mpesa implementation

--- src/providers/mpesa.provider.js
+++ src/providers/mpesa.provider.js
@@ -1,9 +1,19 @@
 // function to process mpesa payments. mpesa implementation and logic goes here
 class MpesaProvider {
     async createPayment(data) {
-      const { amount, currency, reference, transactionId } = data
+      const { amount, phone } = data
+      if (!phone) {
+        throw new Error("Phone number is required for M-Pesa payments")
+      }
       url = https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest
-      const response = await axios.post(url, data)
+      const businessCode = process.env.BUSINESS_SHORT_CODE
+      const password = passkey + businessCode + timestamp
+      const response = await axios.post(url, {
+        body: {
+
+        }
+      })
+
       {
         business-short-code,
         password(passkey + business-short-code + timestamp)- Base64-encoded-string,
@@ -37,13 +47,27 @@ class MpesaProvider {
           const response = await axios.get(url,
           { headers: {
               Authorization: `Basic ${auth}`
-          } 
+            } 
           })
           return response.access_token
       } catch (error) {
           console.error(`Failed to generate access token: ${error.message}`)
           throw error
       }
+  }
+
+  generateTimestamp() {
+
+  }
+
+  generatePassword() {
+    const shortCode = process.env.BUSINESS_SHORT_CODE
+    const passkey = process.env.MPESA_PASSKEY
+    const timestamp = this.generateTimestamp()
+
+    const password = Buffer.from(`${shortCode}:${passkey}:${timestamp}`).toString("base64")
+
+    return password
   }
 }
   
