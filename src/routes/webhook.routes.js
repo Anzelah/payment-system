613a73d336a614f05ae4f2b31ae7da664eb2ae53
Anzelah <jepkoechanzellah@gmail.com>
2026-04-07 12:01:08 +0300

    move the token generation to router

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios")
 const webhookController = require("../controllers/webhook.controller")
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
@@ -23,6 +24,28 @@ router.post("/stripe", express.raw({ type: "application/json" }), async(req, res
 }, webhookController.stripeWebhook)
 
 
-router.post("/mpesa", webhookController.mpesaCallback)
+
+router.post("/mpesa", async(req, res, next) => {
+    const url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    const consumerKey = process.env.MPESA_CONSUMER_KEY
+    const consumerSecret = process.env.MPESA_CONSUMER_SECRET
+
+    // BASE64 encode cnsumer key + secret
+    const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString("base64")
+
+    // get access token
+    try {
+        const response = await axios.get(url,
+        { headers: {
+            Authorization: `Basic ${auth}`
+        } 
+        })
+        req.token = response.access_token
+        next()
+    } catch (error) {
+        console.error(`Failed to generate access token: ${error.message}`)
+        return res.status(401).json({ error: "Incorrect or expired access token"})
+    }
+}, webhookController.mpesaCallback)
 
 module.exports = router;
