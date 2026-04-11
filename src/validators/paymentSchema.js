5229576eed9bc657f468e4b44f2d7b761f70a4af
Anzelah <jepkoechanzellah@gmail.com>
2026-04-11 14:32:26 +0300

    add refine to further validate phone/currency depending on the provider

--- src/validators/paymentSchema.js
+++ src/validators/paymentSchema.js
@@ -8,14 +8,34 @@ const createPaymentSchema = z.object({
 
     currency: z.string()
         .length(3, { message: "Use a valid currency"})
+        .toUpperCase()
         .optional(),
 
-    provider: z.enum(["stripe", "mpesa"]),
+    provider: z.enum(["stripe", "mpesa"], {
+        errorMap: () => ({ message: "Invalid payment provider selected" })
+    }),
 
-    phone: z.number()
+    phone: z.string()
         .regex(/^2547\d{8}$/, "Phone number must be in format 2547XXXXXXXX")
-        .length(12, { message: "Phone number must be 12 digits"})
         .optional()
+
+})
+.refine((data) => {
+    if(data.provider === "stripe" && !data.currency) {
+        return false
+    }
+    return true
+}, {
+    message: "Currency is required for stripe",
+    path: ["currency"]
+}).refine((data) => {
+    if (data.provider === "mpesa" && !data.phone) {
+        return false
+    }
+    return true
+}, {
+    message: "Phone number is required for mpesa",
+    path: ["phone"]
 })
 
 module.exports = { createPaymentSchema }
