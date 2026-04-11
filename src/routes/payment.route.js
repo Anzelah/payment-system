f5c3987a986a8edea6b971f7f71c2be352b793b3
Anzelah <jepkoechanzellah@gmail.com>
2026-04-11 13:52:18 +0300

    add validate middleware to our routes

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const createPayment = require("../controllers/payment.controller")
 const router = express.Router()
+const validateMiddleware = require("../middlewares/validate")
+const { createPaymentSchema } = require("../validators/paymentSchema")
 
-router.post("/", createPayment)
+router.post("/", validateMiddleware(createPaymentSchema), createPayment)
 
 module.exports = router;
