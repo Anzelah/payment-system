52c46312f7baf729a64d99eaf62537121cf7ffc0
Anzelah <jepkoechanzellah@gmail.com>
2026-04-11 13:28:29 +0300

    implement schemas for zod validation

--- src/validators/paymentSchema.js
+++ src/validators/paymentSchema.js
@@ -1,13 +1,21 @@
 const { z } = require("zod")
 
-const PaymentProvider = z.enum(["mpesa", "stripe"]);
-// Prompt validation
-const paySchema = z.object({
-    amount: z.number().positive,
-    currency: z.string().min(2).max(3),
-    provider: PaymentProvider,
-    idempotencyKey: z.string().min(10),
-    phone: z.number().optional()
+// validate all inputs
+const createPaymentSchema = z.object({
+    amount: z
+        .number({ message: "Amount must be a number" })
+        .positive({ message: "Amount must be larger than zero"}),
+
+    currency: z.string()
+        .length(3, { message: "Use a valid currency"})
+        .optional(),
+
+    provider: z.enum(["stripe", "mpesa"]),
+
+    phone: z.number()
+        .regex(/^2547\d{8}$/, "Phone number must be in format 2547XXXXXXXX")
+        .length(12, { message: "Phone number must be 12 digits"})
+        .optional()
 })
 
-module.exports = { paySchema }
+module.exports = { createPaymentSchema }
