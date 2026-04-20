2dcb3565c29a777ec086f8949bd85194d17b1c36
Anzelah <jepkoechanzellah@gmail.com>
2026-04-20 13:49:17 +0300

    add a try catch block

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -2,23 +2,27 @@ const prisma = require("../utils/prisma")
 const getUserMessage = require("../utils/message")
 
 
-async function getTransactionStatus() {
-    const reference = req.params
-    if (!req.params) {
-        return res.status(400).json({ error: "Missing required fieds"})
-    }
+async function getTransactionStatus(req, res) {
+    try {
+        const { reference } = req.params
+        if (!reference) {
+            return res.status(400).json({ error: "Missing required fieds"})
+        }
 
-    const transaction = await prisma.transaction.findUnique({
-        where: { reference }
-    })
-    if (!transaction) {
-        return res.status(404).json({ error: "Could not find the transaction for this reference"})
-    }
+        const transaction = await prisma.transaction.findUnique({
+            where: { reference }
+        })
+        if (!transaction) {
+            return res.status(404).json({ error: "Could not find the transaction for this reference"})
+        }
 
-    res.json({
-        status: transaction.status,
-        message: getUserMessage(transaction.status)
-    })
+        res.json({
+            status: transaction.status,
+            message: getUserMessage(transaction.status)
+        })
+    } catch(error) {
+        res.status(500).json({ error: "Something went wrong" })
+    }
 }
 
 module.exports = getTransactionStatus
