d92b228041aac7b7dfcdfb7bed27ce157f5483c3
Anzelah <jepkoechanzellah@gmail.com>
2026-04-23 10:54:27 +0300

    complete the refunds route

--- src/routes/refund.route.js
+++ src/routes/refund.route.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const router = express.Router()
 const validateMiddleware = require("/middlewares/validate")
+const processRefunds = require("../controllers/refund.controller")
 
-
-router.post("/", )
+router.post("/", processRefunds)
 
 module.exports = router
