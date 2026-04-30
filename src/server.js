d87f02cea9317e753a637a5bfd8411ff19a2ea78
Anzelah <jepkoechanzellah@gmail.com>
2026-04-30 10:19:40 +0300

    enable cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 const PORT = process.env.PORT || 5000
 const routes = require('./routes/index');
 const stripeWebhookRoute = require("./routes/stripeWebhook.routes");
 
 const app = express()
+app.use(cors())
 
 // register stripe webhook before express.json()
 app.use("/api/webhooks/stripe", stripeWebhookRoute)
