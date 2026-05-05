4f8a893d71df501d93230d2a21af1dc5b0179597
Anzelah <jepkoechanzellah@gmail.com>
2026-05-05 18:13:47 +0300

    change the redis url to match upstash

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,9 +1,7 @@
 const Redis = require("ioredis")
 
-const redisConnection = new Redis({
-    host: "127.0.0.1",
-    port: 6379,
-    maxRetriesPerRequest: null,
+const redisConnection = new Redis(process.env.REDIS_URL, {
+    tls: {}
 })
 
 module.exports = redisConnection;
